909091eb0dcc0973e6e7c953acc6177017d084d6
Carl Bebli <beblicarl.cb@gmail.com>
2023-06-15 21:18:23 +0000

    imported Bar component and passed down props

--- src/components/Barchart.jsx
+++ src/components/Barchart.jsx
@@ -1,19 +1,13 @@
 /* eslint-disable react/prop-types */
+
+import Bar from "./Bar";
 export default function Barchart({ data }) {
   const maxValue = Math.max(...data.map((item) => item.amount));
 
   return (
     <div className="bar-chart">
       {data.map((item) => (
-        <div className="bar-chart-bar" key={item.day}>
-          <div className="bar-chart-label">{item.day}</div>
-          <div
-            className={`bar-chart-progress${
-              item.amount === maxValue ? " max-value" : ""
-            }`}
-            style={{ height: `${(item.amount / maxValue) * 100}%` }}
-          ></div>
-        </div>
+        <Bar key={item.day} maxValue={maxValue} item={item} />
       ))}
     </div>
   );
